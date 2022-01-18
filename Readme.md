e10528f72e9298c259057cac3dbbcb3680c99445
Ilya Katz <ilyakatz@gmail.com>
2022-01-18 13:35:11 -0800

    deploy NFT

---
+++ Readme.md
@@ -0,0 +1,11 @@
+This is test NFT using 
+
+https://ethereum.org/en/developers/tutorials/how-to-write-and-deploy-an-nft/
+
+```
+npx hardhat compile
+```
+
+```
+npx hardhat --network ropsten run scripts/deploy.js --verbose
+```
---
+++ contracts/MyNFT.sol
@@ -0,0 +1,34 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() public ERC721("MyNFT", "NFT") {}
+
+    /**
+     *  @param recipient specifies the address that will receive your freshly minted NFT
+     *  @param tokenURI  string that should resolve to a JSON document that describes the NFT's metadata. 
+     *     An NFT's metadata is really what brings it to life, allowing it to have configurable properties, 
+     *     such as a name, description, image, and other attributes
+     */
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
